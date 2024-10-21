abc5b7e70a1e8091553f4fa8fcc65a754563631f
f-howle0528 <f.howle0528@gmail.com>
2024-10-21 15:44:59 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ImageGallery from './ImageGallery';
+import {useRef, useState} from 'react';
 
 function App() {
+const [fetchData, setFetchData] = useState([]);
+const ref = useRef();
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(ref.current.value);
+
+  const endpointURL=`https://pixabay.com/api/?key=46399747-f874a1c505553e1c692356fe6&q=${ref.current.value}&image_type=photo`;
+  fetch(endpointURL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.hits);
+      setFetchData(data.hits);
+    });
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Find Your Favorite Picture</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="Search your picture" ref={ref}/>     
+      </form> 
+      <ImageGallery fetchData={fetchData}/>
     </div>
   );
 }
